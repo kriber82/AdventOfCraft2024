4619436b22120b4c969cb454b617a32597de538d
kriber82 <31168700+kriber82@users.noreply.github.com>
2024-12-05 12:25:14 +0100

    (WIP) create mockk tests, assisted by AI

--- exercise/kotlin/day04/src/test/kotlin/RoutineTest.kt
+++ exercise/kotlin/day04/src/test/kotlin/RoutineTest.kt
@@ -1,10 +1,40 @@
 import io.kotest.core.spec.style.StringSpec
+import io.mockk.mockk
+import io.mockk.every
+import io.mockk.verify
+import routine.*
 
-class RoutineTest : StringSpec({
-    "start routine with Mockk" {
+class RoutineTestWithMockK : StringSpec({
 
+    lateinit var emailService: EmailService
+    lateinit var scheduleService: ScheduleService
+    lateinit var reindeerFeeder: ReindeerFeeder
+    lateinit var schedule: Schedule
+    lateinit var routine: Routine
+
+    beforeTest {
+        emailService = mockk(relaxed = true)
+        scheduleService = mockk(relaxed = true)
+        reindeerFeeder = mockk(relaxed = true)
+        schedule = Schedule()
+
+        every { scheduleService.todaySchedule() } returns schedule
+
+        routine = Routine(emailService, scheduleService, reindeerFeeder)
     }
 
-    "start routine with manual test doubles" {
+    "test start" {
+        routine.start()
+
+        verify { scheduleService.organizeMyDay(schedule) }
+        verify { reindeerFeeder.feedReindeers() }
+        verify { emailService.readNewEmails() }
+        verify { scheduleService.continueDay() }
     }
+
+    /*
+    "should feed reindeers during routine" {
+        // Test case implementation goes here
+    }
+    */
 })
