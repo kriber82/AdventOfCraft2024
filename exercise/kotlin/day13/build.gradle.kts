bda19bb416714eacb8c66817ee7cf3d12891f85d
Kristian Bergmann <kristian.bergmann@codecentric.de>
2024-12-17 12:50:53 +0100

    added pitest

--- exercise/kotlin/day13/build.gradle.kts
+++ exercise/kotlin/day13/build.gradle.kts
@@ -1,8 +1,10 @@
+import info.solidsoft.gradle.pitest.PitestPluginExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("jvm") version "1.9.21"
     application
+    id("info.solidsoft.pitest") version "1.15.0"
 }
 
 repositories {
@@ -14,16 +16,36 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-property-jvm:$kotestVersion")
     testImplementation(kotlin("test"))
+
+    testImplementation("io.kotest.extensions:kotest-extensions-pitest:1.2.0")
 }
+
+pitest {
+    targetClasses.set(setOf("santamarket.model.*")) //by default "${project.group}.*"
+    pitestVersion.set("1.12.0") //not needed when a default PIT version should be used
+    threads.set(4)
+    outputFormats.set(setOf("XML", "HTML"))
+    timestampedReports.set(false)
+    verbose.set(true)
+}
+
+/*
+// Assuming that you have already configured the Gradle/Maven extension
+configure<PitestPluginExtension> {
+    // testPlugin.set("Kotest")    // needed only with old PIT <1.6.7, otherwise having kotest-extensions-pitest on classpath is enough
+    targetClasses.set(listOf("santamarket.model.*"))
+}
+
+ */
 
 tasks.test {
     useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
