dd3cb358235815a4dcb9ebbd8560fe7303e3478d
Kristian Bergmann <kristian.bergmann@codecentric.de>
2024-12-21 12:10:09 +0100

    refactor: made data class vs class usage consistent

--- exercise/kotlin/day17/src/main/kotlin/eid/ParsingError.kt
+++ exercise/kotlin/day17/src/main/kotlin/eid/ParsingError.kt
@@ -6,11 +6,11 @@ sealed interface ParsingError {
     data class InputTooShort(override val message: String = "Input is too short for an EID (must be 8 characters long)") : ParsingError
     data class InputTooLong(override val message: String = "Input is too long for an EID (must be 8 characters long)") : ParsingError
 
-    data class InvalidElfGender(private val genderSubstring: String): ParsingError {
+    class InvalidElfGender(genderSubstring: String): ParsingError {
         override val message = "$genderSubstring dose not represent a valid gender (must be 1, 2 or 3)"
     }
 
-    data class InvalidYear(private val yearSubstring: String): ParsingError {
+    class InvalidYear(yearSubstring: String): ParsingError {
         override val message = "$yearSubstring dose not represent a valid year (must be between 00 and 99)"
     }
 
